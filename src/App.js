51a631eb72a4575fc7a6ab1deebe05361e010903
Raj Sen <rajsen89611@gmail.com>
2021-09-03 07:23:13 +0000

    work with object

--- src/App.js
+++ src/App.js
@@ -7,21 +7,29 @@ function call(){
 }
 
 const App = () => {
-  const [count, setCount] = useState(() => call())
+  // const [count, setCount] = useState(() => call())
+  const [state, setState] = useState({count: 4, theme: 'blue'})
 
   const decrementCount = () =>{
-    let  count 
-    setCount(prevCount => prevCount-1)
-    console.log(count);
+    // setCount(prevCount => prevCount-1)
+    // setCount(prevCount => prevCount-1)
+
+    setState(prevState => {
+      return { ...prevState, count: prevState.count -1}
+    })
+
+
   }
 
   const incrementCount = () => {
-    setCount(prevCount => prevCount+1)
+    // setCount(prevCount => prevCount+1)
   }
   return (
     <div>
       <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
+      {/* <span>{count}</span> */}
+      <span>{state.count}</span>
+      <span>{state.theme}</span>
       <button onClick={incrementCount}>+</button>
     </div>
   )
